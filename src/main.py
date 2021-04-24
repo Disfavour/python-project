c00601410f8f0bca4ece686337aa49e10739f80c
Disfavour <disfavour228@gmail.com>
2021-04-24 17:47:24 +0300

    Simple echo bot

--- src/main.py
+++ src/main.py
@@ -1,8 +1,23 @@
-"""Module-project."""
-def main():
-    """Do something."""
-    pass
+import aiogram
+from aiogram.utils.exceptions import BotBlocked
+
+
+bot = aiogram.Bot(token="1796535419:AAFMLfG35KAugeQqhDmWdApx5pZMnK43fDU")
+dp = aiogram.Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo(message: aiogram.types.Message):
+    await message.answer(message.text)
+
+
+@dp.errors_handler(exception=BotBlocked)
+async def error_bot_blocked(update: aiogram.types.Update, exception: BotBlocked):
+    """Если заблокировали бота."""
+    # Здесь можно как-то обработать блокировку, например, удалить пользователя из БД
+    print(f"Меня заблокировал пользователь!\nСообщение: {update}\nОшибка: {exception}")
+    return True
 
 
 if __name__ == "__main__":
-    main()
+    aiogram.executor.start_polling(dp, skip_updates=True)
