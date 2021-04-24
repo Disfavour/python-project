5a09b8b798f4db17aeadd3ef602c17d9f36c1ab5
Disfavour <disfavour228@gmail.com>
2021-04-24 18:00:08 +0300

    options for Гороскоп

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 import aiogram
 from aiogram.utils.exceptions import BotBlocked
 
-
 bot = aiogram.Bot(token="1796535419:AAFMLfG35KAugeQqhDmWdApx5pZMnK43fDU")
 dp = aiogram.Dispatcher(bot)
 
-functions = ["Гороскоп", "Погода", "Новости",
-             "Напоминания", "Счётчик расходов/доходов", "Списки покупок",
-             "Список ближайших премьер", "Рецепты по ингредиентам"]
+options = ["Гороскоп", "Погода", "Новости",
+           "Напоминания", "Счётчик расходов/доходов", "Списки покупок",
+           "Список ближайших премьер", "Рецепты по ингредиентам"]
+
+zodiac_signs = ["Овен", "Телец", "Близнецы", "Рак", "Лев", "Дева", "Весы",
+                "Скорпион", "Змееносец", "Стрелец", "Козерог", "Водолей", "Рыбы"]
 
 
 @dp.message_handler(commands="help")
@@ -31,9 +33,26 @@ async def cmd_dice(message: aiogram.types.Message):
 async def cmd_start(message: aiogram.types.Message):
     """Показать функции пользователю."""
     keyboard = aiogram.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    buttons = functions
+    buttons = options
     keyboard.add(*buttons)
     await message.answer("Выберите функцию", reply_markup=keyboard)
+
+
+@dp.message_handler(regexp=r"^Гороскоп$")
+async def goroskop(message: aiogram.types.Message):
+    """Показать все знаки гороскопа."""
+    keyboard = aiogram.types.InlineKeyboardMarkup(row_width=3)
+    tmp = []
+    for item in zodiac_signs:
+        tmp.append(aiogram.types.InlineKeyboardButton(text=item, callback_data=item))
+    keyboard.add(*tmp)
+
+    await message.answer("Выберите ваш знак зодиака", reply_markup=keyboard)
+
+
+@dp.callback_query_handler(text=zodiac_signs)
+async def send_random_value(call: aiogram.types.CallbackQuery):
+    await call.message.answer(call.data)
 
 
 @dp.message_handler()
