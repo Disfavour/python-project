530e36b3c2d5278dae52ca35d14002e1f685aaef
Disfavour <disfavour228@gmail.com>
2021-05-08 00:53:15 +0300

    Сделал правильные докстринги под сфинкс

--- src/afisha.py
+++ src/afisha.py
@@ -1,3 +1,4 @@
+"""Обработка афиши."""
 import aiogram
 import stuff
 import parsing
@@ -7,14 +8,22 @@ afisha_obj = parsing.AFISHA()
 
 
 async def afisha_handle(message: aiogram.types.Message):
-    """Показать виды афиши."""
+    """
+    Предоставить выбор вида афиши.
+
+    :param message: сообщение
+    """
     options = afisha_obj.options
     afisha_obj.setup()
     await message.answer("Что именно?", reply_markup=stuff.get_inline_keyboard_from_list(options))
 
 
 async def afisha_handle_callback(call: aiogram.types.CallbackQuery):
-    """Обработать колбек."""
+    """
+    Обработать нажатие кнопки.
+
+    :param call: вызов
+    """
     action = call.data
     data = afisha_obj.get_data_smart(action)
     if data:
@@ -27,11 +36,15 @@ async def afisha_handle_callback(call: aiogram.types.CallbackQuery):
                     item, parse_mode=aiogram.types.ParseMode.HTML, reply_markup=stuff.get_more_inline_keyboard(action))
     else:
         await call.message.delete_reply_markup()
-
     await call.answer()
 
 
 def register_handlers(dp: aiogram.Dispatcher) -> None:
+    """
+    Зарегистрировать обработчики.
+
+    :param dp: диспетчер
+    """
     dp.register_message_handler(afisha_handle, regexp=r"^Афиша$")
     dp.register_callback_query_handler(afisha_handle_callback, text=afisha_obj.options)
 
