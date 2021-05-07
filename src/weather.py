ed96afb6b14f9684520b3ca5414eada4b593ff31
Disfavour <disfavour228@gmail.com>
2021-05-08 00:54:21 +0300

    Сделал правильные докстринги под сфинкс

--- src/weather.py
+++ src/weather.py
@@ -1,3 +1,4 @@
+"""Обработка погоды."""
 import aiogram
 import parsing
 
@@ -6,12 +7,21 @@ weather_obj = parsing.WEATHER()
 
 
 async def weather_handle(message: aiogram.types.Message):
-    """Показать все знаки гороскопа."""
+    """
+    Показать погоду.
+
+    :param message: сообщение
+    """
     text = weather_obj.get_data_smart()
     await message.answer(text, parse_mode=aiogram.types.ParseMode.HTML)
 
 
 def register_handlers(dp: aiogram.Dispatcher) -> None:
+    """
+    Зарегистрировать обработчики.
+
+    :param dp: диспетчер
+    """
     dp.register_message_handler(weather_handle, regexp=r"^Погода$")
 
 
