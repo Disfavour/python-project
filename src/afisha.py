c7c2fc5af781b083239ee5b26bfce26783402b7f
Disfavour <disfavour228@gmail.com>
2021-05-07 22:10:55 +0300

    small optimizations

--- src/afisha.py
+++ src/afisha.py
@@ -1,90 +1,39 @@
 import aiogram
-from bot import dp
-from aiogram.utils.callback_data import CallbackData
+import stuff
 import parsing
-import aiogram.utils.markdown as fmt
 
 
-afisha = parsing.AFISHA()
-callback_afisha = CallbackData("afisha", "action")
-options_afisha = ["Кино", "Театр", "Концерт"]
-user_data = {}
+afisha_obj = parsing.AFISHA()
 
 
-@dp.message_handler(regexp=r"^Афиша$")
-async def horoscope(message: aiogram.types.Message):
-    """Показать все знаки гороскопа."""
-    keyboard = aiogram.types.InlineKeyboardMarkup()
-    for item in options_afisha:
-        keyboard.add(aiogram.types.InlineKeyboardButton(text=item, callback_data=callback_afisha.new(action=item)))
-
-    afisha.cinema_count = 0
-    afisha.theatre_count = 0
-    afisha.concert_count = 0
-    await message.answer("Выбирайте:", reply_markup=keyboard)
+async def afisha_handle(message: aiogram.types.Message):
+    """Показать виды афиши."""
+    options = afisha_obj.options
+    afisha_obj.setup()
+    await message.answer("Что именно?", reply_markup=stuff.get_inline_keyboard_from_list(options))
 
 
-@dp.callback_query_handler(callback_afisha.filter(action=[options_afisha[0]]))
-async def handle_horoscope_callback(call: aiogram.types.CallbackQuery, callback_data: dict):
-    """Кино"""
-    keyboard = aiogram.types.InlineKeyboardMarkup()
-    keyboard.add(aiogram.types.InlineKeyboardButton(
-        text="Ещё", callback_data=callback_afisha.new(action=options_afisha[0])))
-
-    links = afisha.get_links_cinema()
-    if links:
+async def afisha_handle_callback(call: aiogram.types.CallbackQuery):
+    """Обработать колбек."""
+    action = call.data
+    data = afisha_obj.get_data_smart(action)
+    if data:
         await call.message.delete_reply_markup()
-        for link in links:
-            if link == links[-1]:
-                await call.message.answer(link, reply_markup=keyboard)
+        for item in data:
+            if item != data[-1]:
+                await call.message.answer(item, parse_mode=aiogram.types.ParseMode.HTML)
             else:
-                await call.message.answer(link)
+                await call.message.answer(
+                    item, parse_mode=aiogram.types.ParseMode.HTML, reply_markup=stuff.get_more_inline_keyboard(action))
     else:
-        call.message.delete_reply_markup()
+        await call.message.delete_reply_markup()
 
     await call.answer()
 
 
-@dp.callback_query_handler(callback_afisha.filter(action=[options_afisha[1]]))
-async def handle_horoscope_callback(call: aiogram.types.CallbackQuery, callback_data: dict):
-    """Театр"""
-    keyboard = aiogram.types.InlineKeyboardMarkup()
-    keyboard.add(aiogram.types.InlineKeyboardButton(
-        text="Ещё", callback_data=callback_afisha.new(action=options_afisha[1])))
-
-    links = afisha.get_links_theatre()
-    if links:
-        await call.message.delete_reply_markup()
-        for link in links:
-            if link == links[-1]:
-                await call.message.answer(link, reply_markup=keyboard)
-            else:
-                await call.message.answer(link)
-    else:
-        call.message.delete_reply_markup()
-
-    await call.answer()
-
-
-@dp.callback_query_handler(callback_afisha.filter(action=[options_afisha[2]]))
-async def handle_horoscope_callback(call: aiogram.types.CallbackQuery, callback_data: dict):
-    """Концерт"""
-    keyboard = aiogram.types.InlineKeyboardMarkup()
-    keyboard.add(aiogram.types.InlineKeyboardButton(
-        text="Ещё", callback_data=callback_afisha.new(action=options_afisha[2])))
-
-    links = afisha.get_links_concert()
-    if links:
-        await call.message.delete_reply_markup()
-        for link in links:
-            if link == links[-1]:
-                await call.message.answer(f"Спрятал ссылку {fmt.hide_link(link)}", parse_mode=aiogram.types.ParseMode.HTML, reply_markup=keyboard)
-            else:
-                await call.message.answer(link)
-    else:
-        call.message.delete_reply_markup()
-
-    await call.answer()
+def register_handlers(dp: aiogram.Dispatcher) -> None:
+    dp.register_message_handler(afisha_handle, regexp=r"^Афиша$")
+    dp.register_callback_query_handler(afisha_handle_callback, text=afisha_obj.options)
 
 
 if __name__ == "__main__":
